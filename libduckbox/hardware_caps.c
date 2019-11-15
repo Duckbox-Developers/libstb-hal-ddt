a0f6dbc96ec82f7181e12ed9d9e33b24e738fccc
Frankenstone <dampf_acc@yahoo.com>
2019-11-15 18:02:53 +0100

    remove, cleanup ferguson arivalink200

--- libduckbox/hardware_caps.c
+++ libduckbox/hardware_caps.c
@@ -246,17 +246,6 @@ hw_caps_t *get_hwcaps(void)
 			caps.display_type = HW_DISPLAY_LINE_TEXT;
 			caps.display_xres = 8;
 		}
-		else if (!strncmp(buf, "arivalink200", 12)) {
-			strcpy(caps.boxvendor, "DUCKBOX");
-			strcpy(caps.boxname, buf);
-			caps.can_shutdown = 0;
-			caps.has_HDMI = 1;
-			caps.has_SCART = 1;
-			caps.can_cec = 1;
-			caps.has_fan = 0;
-			caps.has_CI = 2;
-			caps.display_can_set_brightness = 0;
-		}
 		else {
 			strcpy(caps.boxvendor, "unknown");
 			strcpy(caps.boxname, buf);
