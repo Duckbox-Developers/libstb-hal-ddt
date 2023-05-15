871db2928ec24e2e0cb1a3ffe0aab008fa129b41
BPanther <bpanther_ts@hotmail.com>
2023-05-15 22:46:21 +0200

    unused gstreamer removed

--- include/playback_hal.h
+++ include/playback_hal.h
@@ -4,22 +4,14 @@
 #elif HAVE_SPARK_HARDWARE
 #include "../libspark/playback_libeplayer3.h"
 #elif HAVE_ARM_HARDWARE
-#if ENABLE_GSTREAMER_10
-#include "../libarmbox/playback_gst.h"
-#else
 #include "../libarmbox/playback_libeplayer3.h"
-#endif
 #elif HAVE_MIPS_HARDWARE
 #include "../libmipsbox/playback_libeplayer3.h"
 #elif HAVE_GENERIC_HARDWARE
 #if BOXMODEL_RASPI
 #include "../libraspi/playback_lib.h"
 #else
-#if ENABLE_GSTREAMER
-#include "../libgeneric-pc/playback_gst.h"
-#else
 #include "../libgeneric-pc/playback_lib.h"
-#endif
 #endif
 #else
 #error no valid hardware defined
