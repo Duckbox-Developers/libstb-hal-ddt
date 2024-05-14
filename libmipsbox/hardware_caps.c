168e73c6ce6bb59bc751c9256e079f27a8bc9538
BPanther <bpanther_ts@hotmail.com>
2024-05-15 01:42:48 +0200

    mips dm820 added

--- libmipsbox/hardware_caps.c
+++ libmipsbox/hardware_caps.c
@@ -123,6 +123,24 @@ hw_caps_t *get_hwcaps(void)
 	strcpy(caps.boxvendor, "DM");
 	strcpy(caps.boxname, "8000HD");
 	strcpy(caps.boxarch, "BCM7400D2");
+#elif BOXMODEL_DM820
+	initialized = 1;
+	caps.has_CI = 0;
+	caps.can_cec = 1;
+	caps.can_shutdown = 1;
+	caps.display_xres = 96;
+	caps.display_yres = 64;
+	caps.display_type = HW_DISPLAY_GFX;
+	caps.display_can_deepstandby = 1;
+	caps.display_can_set_brightness = 1;
+	caps.display_has_statusline = 1;
+	caps.display_has_colon = 0;
+	caps.has_button_timer = 1;
+	caps.has_HDMI = 2;
+	caps.has_HDMI_input = 1;
+	strcpy(caps.boxvendor, "DM");
+	strcpy(caps.boxname, "820HD");
+	strcpy(caps.boxarch, "BCM7434");
 #else // generic mips box
 	initialized = 1;
 	caps.has_CI = 1;
